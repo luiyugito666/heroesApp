5f3cde127d47c9e4d8c58b7a9b0db023020f65d3
jorge luis yucra <jorgeyh7@gmail.com>
2023-07-13 18:33:13 -0500

    implementacion del formulario reactivo

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -8,6 +11,17 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('',
+      {nonNullable: true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+
+  });
   public publishers = [
     
     {
@@ -17,4 +31,21 @@ export class NewPageComponent {
       
     }
   ]
+
+  constructor(private heroesService:HeroesService) { }
+
+
+  get currentHero(): Hero { 
+    const hero = this.heroForm.value as Hero;
+    return hero;
+    
+  }
+
+  onSubmit(): void { 
+    if (this.heroForm.invalid) return;
+
+  }
+
+
+
 }
